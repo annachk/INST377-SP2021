8951b0e4764d4c275a84e86604c7c301c24aae7d
annachk <annachk@terpmail.umd.edu>
2021-02-18 22:09:01 -0300

    updated lab 4

--- public/labs/lab_4/script.js
+++ public/labs/lab_4/script.js
@@ -3,17 +3,3 @@ function onload() {
 }
 
 window.onload = onload;
-
-// module.exports = (on, config) => {
-//   on('task', {
-//     log (message) {
-//       console.log(message)
-  
-//       return null
-//     }
-//   })
-// }
-
-// cy.task("htmlvalidateOptions", null, { log: false }).then((options) => {
-//     cy.document({ log: false }).then((document) => {
-//         const source = utils_1.getPageSource(document)}
